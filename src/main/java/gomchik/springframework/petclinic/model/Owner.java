663a136f627071f56461c63d17e09bf9f42c37b1
Vivek Bhargav <vbbharga@amazon.com>
2020-04-12 03:29:49 +0530

    aadding POJOs.

---
+++ src/main/java/gomchik/springframework/petclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package gomchik.springframework.petclinic.model;
+
+/**
+ * @author vbbharga on 12/04/20
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/gomchik/springframework/petclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package gomchik.springframework.petclinic.model;
+
+/**
+ * @author vbbharga on 12/04/20
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
